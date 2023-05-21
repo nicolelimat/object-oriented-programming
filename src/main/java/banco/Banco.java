737cd84cd32920d0e2a792078eedec40bc341b2f
nicolelimat <nicolelima.eu@gmail.com>
2023-05-21 16:17:57 -0300

    LAB#08 - 3/4 - Implementação de pesquisa de clientes pelo nome e sobrenome

--- src/main/java/banco/Banco.java
+++ src/main/java/banco/Banco.java
@@ -17,6 +17,17 @@ public class Banco {
         return clientes.get(indice);
     }
 
+    public ArrayList<Cliente> getCliente(String nome, String sobrenome){
+        ArrayList<Cliente> resultados = new ArrayList<>();
+        for(int i = 0; i < clientes.size(); i++){
+            Cliente cliente = clientes.get(i);
+            if((cliente.getNome() == nome) && (cliente.getSobrenome() == sobrenome)){
+                resultados.add(cliente);
+            }
+        }
+        return resultados;
+    }
+
     public int getNumeroDeClientes(){
         return clientes.size();
     }
